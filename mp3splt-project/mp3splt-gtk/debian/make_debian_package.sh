34ae1b0b2a8c6578cdcb8d0e10a0a4f7ae4bde92
io_alex_2004 <io_alex_2004@15f1896c-5e77-45dc-b76d-d072f9a9b3a2>
2006-09-27 21:08:38 +0000

    added hurd specific debian package creation script for mp3splt-gtk

---
+++ mp3splt-project/mp3splt-gtk/debian/make_debian_package.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#we move in the current script directory
+script_dir=$(readlink -f $0)
+script_dir=${script_dir%\/*.sh}
+PROGRAM_DIR=$script_dir/../
+cd $PROGRAM_DIR
+
+#set necessary flags
+export CFLAGS="$CFLAGS -I/tmp/temp/usr/include"
+export LDFLAGS="$LDFLAGS -L/tmp/temp/usr/lib"
+
+#we compile
+./autogen.sh && \
+./configure --enable-bmp --prefix=/usr && \
+make clean && \
+make && \
+#we create the debian package
+./debian/rules binary
