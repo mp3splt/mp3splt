7acc8e09d95d7a099267c327a54b944faead6dc6
io_alex_2004 <io_alex_2004@15f1896c-5e77-45dc-b76d-d072f9a9b3a2>
2008-12-13 01:02:39 +0000

    new mp3splt-gtk installer + created scripts to auto generate '.nsi' script for safer uninstall; must be tested

--- mp3splt-project/trunk/mp3splt-gtk/windows/make_windows_installer.sh
+++ mp3splt-project/trunk/mp3splt-gtk/windows/make_windows_installer.sh
@@ -21,16 +21,25 @@ else
   DLL_SUFFIX="-0"
 fi
 
+#we compile the locales
+mv -f fr_locales fr_locales_old &>/dev/null
+mkdir -p fr_locales
+wine `pwd`/../../../libs/bin/msgfmt -o ./fr_locales/mp3splt-gtk.mo ../po/fr.po || exit 1
+
+cd ../../../libs &&\
+tar jxf mp3splt-gtk_runtime.tar.bz2 -C ../trunk || exit 1 &&\
+cd -
+
+
 #generate the '.nsi' installer script
 
 WIN_INSTALLER_FILE="win32_installer.nsi"
 
-TMP_GENERATED_FILES_FILE='.mp3splt_tmp_uninstall_files.txt'
-TMP_CREATED_DIRECTORIES_FILE='.mp3splt_tmp_uninstall_directories.txt'
+TMP_GENERATED_FILES_FILE='.mp3splt-gtk_tmp_uninstall_files.txt'
+TMP_CREATED_DIRECTORIES_FILE='.mp3splt-gtk_tmp_uninstall_directories.txt'
 
 echo '' > $TMP_GENERATED_FILES_FILE
 echo '' > $TMP_CREATED_DIRECTORIES_FILE
- 
 
 CURRENT_OUT_PATH=""
 
@@ -58,24 +67,81 @@ function copy_files()
 #directories in a temporary file for generating the uninstaller
 function create_directory()
 {
-  DIR=$@
+  DIR=$1
   echo '    CreateDirectory '$DIR >> $WIN_INSTALLER_FILE
   echo "  RmDir $DIR" >> $TMP_CREATED_DIRECTORIES_FILE
+}
+
+#-generate function files recursively from the director passed as parameter
+function recursive_copy_files_from_directory()
+{
+  DIR=$1
+
+  cd $DIR
+
+  directories=$(find . -type d)
+  files=$(find . -type f)
+
+  #prepare directories to remove at the uninstall
+  find . -type d -exec echo '  RmDir ''{}' \; | grep -v 'RmDir \.$' | \
+    sed 's+RmDir \.+RmDir $INSTDIR+; s+/+\\+g' | tac >> $script_dir/$TMP_CREATED_DIRECTORIES_FILE
+
+  #prepare files to remove at the uninstall
+  find . -type f -exec echo '  Delete $INSTDIR'"\\"'{}' \; | \
+    sed 's+$INSTDIR\\\.+$INSTDIR+; s+/+\\+g' >> $script_dir/$TMP_GENERATED_FILES_FILE
+
+  cd -
+
+  echo '' >> $WIN_INSTALLER_FILE
+
+  #create the directories
+  for cur_dir in ${directories};do
+
+    windows_dir=$(echo ${cur_dir#\.\/} | sed 's+/+\\+g')
+    windows_dir="\\"$windows_dir
+
+    #prepare windows_dir as '\directories' or empty string
+    if [[ $windows_dir = "\\." ]];then
+      windows_dir=""
+    fi
+
+    #don't re-create the $INSTDIR directory
+    if [[ ! -z $windows_dir ]];then
+      echo '    CreateDirectory $INSTDIR'$windows_dir >> $WIN_INSTALLER_FILE
+    fi
+    
+    #copy files from the current directory (not recursively)
+    cd $DIR/$cur_dir
+    files=$(find . -maxdepth 1 -type f)
+    cd -
+
+    if [[ ! -z $files ]];then
+
+      #set the outpath as the current directory
+      set_out_path '$INSTDIR'$windows_dir
+
+      for cur_file in ${files};do
+        file=$(echo ${cur_file#\.\/} | sed 's+/+\\+g')
+        echo '    File ${MP3SPLT_PATH}\mp3splt-gtk_runtime'$windows_dir"\\"$file >> $WIN_INSTALLER_FILE
+      done
+
+    fi
+
+  done
 }
 
 #main options
 echo "!include MUI2.nsh
 
 ;program variables
-!define VERSION \"$MP3SPLT_VERSION\"
-!define PROGRAM_NAME \"mp3splt\"" > $WIN_INSTALLER_FILE
+!define VERSION \"${MP3SPLT_GTK_VERSION}\"
+!define PROGRAM_NAME \"mp3splt-gtk\"" > $WIN_INSTALLER_FILE
 
 if [[ -z $we_dont_cross_compile ]];then
   echo "!define MP3SPLT_PATH \"c:/mp3splt_mingw`pwd`/../..\"" >> $WIN_INSTALLER_FILE
 else 
   echo "!define MP3SPLT_PATH \"`pwd`/../..\"" >> $WIN_INSTALLER_FILE
 fi
-
 
 #generates the Delete file1.txt for all the installed files
 # and after, RmDir dir for all the created directories
@@ -87,24 +153,24 @@ function generate_uninstall_files_dirs()
 
 echo '
 ;name of the program
-Name "mp3splt ${VERSION}"
+Name "mp3splt-gtk ${VERSION}"
 ;file to write
-OutFile "../../mp3splt_'$MP3SPLT_VERSION'_'$ARCH'.exe"
+OutFile "../../mp3splt-gtk_${VERSION}_'$ARCH'.exe"
 ;installation directory
-InstallDir $PROGRAMFILES\mp3splt
+InstallDir $PROGRAMFILES\mp3splt-gtk
 
 BrandingText " "
 
 ;interface settings
-!define MUI_ICON ${MP3SPLT_PATH}/newmp3splt/windows/mp3splt.ico
-!define MUI_UNICON ${MP3SPLT_PATH}/newmp3splt/windows/mp3splt.ico
+!define MUI_ICON ${MP3SPLT_PATH}/mp3splt-gtk/windows/mp3splt-gtk.ico
+!define MUI_UNICON ${MP3SPLT_PATH}/mp3splt-gtk/windows/mp3splt-gtk.ico
 !define MUI_COMPONENTSPAGE_NODESC
 ShowInstDetails "show"
 ShowUninstDetails "show"
 SetCompressor /SOLID "lzma"
 
 ;install pages
-!insertmacro MUI_PAGE_LICENSE ${MP3SPLT_PATH}\newmp3splt\COPYING
+!insertmacro MUI_PAGE_LICENSE ${MP3SPLT_PATH}\mp3splt-gtk\COPYING
 !insertmacro MUI_PAGE_COMPONENTS
 !insertmacro MUI_PAGE_DIRECTORY
 !insertmacro MUI_PAGE_INSTFILES
@@ -121,25 +187,24 @@ SetCompressor /SOLID "lzma"
 
 #main section
 MAIN_SECTION_FILES="
-newmp3splt\src\mp3splt.exe
+mp3splt-gtk\src\mp3splt-gtk.exe
 libmp3splt\src\.libs\libmp3splt${DLL_SUFFIX}.dll
 libltdl3.dll
-zlib1.dll
+mp3splt-gtk\src\mp3splt-gtk.png
+mp3splt-gtk\src\mp3splt-gtk_ico.png
+mp3splt-gtk\windows\mp3splt-gtk.ico
 "
 
 echo '
 ;main installation section
-Section "mp3splt (with libmp3splt)" main_section
+Section "mp3splt-gtk (with libmp3splt)" main_section
 ' >> $WIN_INSTALLER_FILE
 
 set_out_path '$INSTDIR'
 copy_files $MAIN_SECTION_FILES
+recursive_copy_files_from_directory "../../mp3splt-gtk_runtime"
 
 echo '
-  FileOpen $9 mp3splt.bat w
-  FileWrite $9 '\''cmd /K "cd $INSTDIR & .\mp3splt.exe"'\''
-  FileClose $9
-
   WriteUninstaller "${PROGRAM_NAME}_uninst.exe"
 
   WriteRegStr HKLM "Software\${PROGRAM_NAME}\" "UninstallString" \
@@ -147,7 +212,7 @@ echo '
 
   ;add to Add/Remove programs
   WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PROGRAM_NAME}" \
-    "DisplayName" "mp3splt"
+    "DisplayName" "mp3splt-gtk"
   WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\${PROGRAM_NAME}" \
     "UninstallString" "$INSTDIR\${PROGRAM_NAME}_uninst.exe"
 
@@ -199,25 +264,24 @@ SubSectionEnd' >> $WIN_INSTALLER_FILE
 
 #mp3splt doc section
 MP3SPLT_DOC_FILES="
-newmp3splt\README
-newmp3splt\doc\manual.html
-newmp3splt\COPYING
-newmp3splt\ChangeLog
-newmp3splt\INSTALL
-newmp3splt\NEWS
-newmp3splt\TODO
-newmp3splt\AUTHORS
+mp3splt-gtk\README
+mp3splt-gtk\COPYING
+mp3splt-gtk\ChangeLog
+mp3splt-gtk\INSTALL
+mp3splt-gtk\NEWS
+mp3splt-gtk\TODO
+mp3splt-gtk\AUTHORS
 "
 
 echo '
 ;main documentation section
 SubSection /e "Documentation" documentation_section
 
-  Section "mp3splt documentation" mp3splt_doc_section
+  Section "mp3splt-gtk documentation" mp3splt_gtk_doc_section
 ' >> $WIN_INSTALLER_FILE
 
-create_directory '$INSTDIR\mp3splt_doc'
-set_out_path '$INSTDIR\mp3splt_doc'
+create_directory '$INSTDIR\mp3splt-gtk_doc'
+set_out_path '$INSTDIR\mp3splt-gtk_doc'
 copy_files $MP3SPLT_DOC_FILES
 
 echo '
@@ -254,54 +318,51 @@ echo '
 Section "Start Menu Shortcuts" menu_shortcuts_section
 ' >> $WIN_INSTALLER_FILE
 
-create_directory '$SMPROGRAMS\mp3splt'
+create_directory '$SMPROGRAMS\mp3splt-gtk'
 
-echo '  CreateShortCut "$SMPROGRAMS\mp3splt\Mp3splt.lnk" "$INSTDIR\mp3splt.bat" "" ""
-	CreateShortCut "$SMPROGRAMS\mp3splt\uninstall.lnk" "$INSTDIR\mp3splt_uninst.exe" "" "$INSTDIR\mp3splt_uninst.exe" 0
+echo '  CreateShortCut "$SMPROGRAMS\mp3splt-gtk\mp3splt-gtk.lnk" "$INSTDIR\mp3splt-gtk.exe" "" "$INSTDIR\mp3splt-gtk.ico" 0
+  CreateShortCut "$SMPROGRAMS\mp3splt-gtk\uninstall.lnk" "$INSTDIR\mp3splt-gtk_uninst.exe" "" "$INSTDIR\mp3splt-gtk_uninst.exe" 0
 
-  ;if mp3splt_doc_section is selected, add mp3splt_doc link
-  SectionGetFlags ${mp3splt_doc_section} $0
+  ;if mp3splt_gtk__doc_section is selected, add mp3splt_doc link
+  SectionGetFlags ${mp3splt_gtk_doc_section} $0
   IntOp $1 $0 & ${SF_SELECTED}
   IntCmp 0 $1 after_doc_shortcut
-  CreateShortCut "$SMPROGRAMS\mp3splt\mp3splt_doc.lnk" "$INSTDIR\mp3splt_doc" "" "$INSTDIR\mp3splt_doc" 
+  CreateShortCut "$SMPROGRAMS\mp3splt-gtk\mp3splt_gtk_doc.lnk" "$INSTDIR\mp3splt_gtk_doc" "" "$INSTDIR\mp3splt_gtk_doc" 
   after_doc_shortcut:
 
   ;if libmp3splt_doc_section is selected, add libmp3splt_doc link
   SectionGetFlags ${libmp3splt_doc_section} $0
   IntOp $1 $0 & ${SF_SELECTED}
   IntCmp 0 $1 after_lib_doc_shortcut
-  CreateShortCut "$SMPROGRAMS\mp3splt\libmp3splt_doc.lnk" "$INSTDIR\libmp3splt_doc"	"" "$INSTDIR\libmp3splt_doc" 
+  CreateShortCut "$SMPROGRAMS\mp3splt-gtk\libmp3splt_doc.lnk" "$INSTDIR\libmp3splt_doc"	"" "$INSTDIR\libmp3splt_doc" 
   after_lib_doc_shortcut:
 
 SectionEnd
 
-
 ;desktop shortcut
 Section "Desktop Shortcut" desktop_shortcut_section
 
-	CreateShortCut "$DESKTOP\mp3splt.lnk" "$INSTDIR\mp3splt.bat" "" ""  
+	CreateShortCut "$DESKTOP\mp3splt-gtk.lnk" "$INSTDIR\mp3splt-gtk.exe" "" "$INSTDIR\mp3splt-gtk.ico"
 
 SectionEnd' >> $WIN_INSTALLER_FILE
 
 
 echo '
 ;uninstallation section
-Section "Uninstall"
-
-  Delete $INSTDIR\mp3splt.bat' >> $WIN_INSTALLER_FILE
+Section "Uninstall"' >> $WIN_INSTALLER_FILE
 
 generate_uninstall_files_dirs
 
 echo '
   ;menu shortcuts
-  Delete $SMPROGRAMS\mp3splt\mp3splt_doc.lnk
-  Delete $SMPROGRAMS\mp3splt\libmp3splt_doc.lnk
-  Delete $SMPROGRAMS\mp3splt\Mp3splt.lnk
-  Delete $SMPROGRAMS\mp3splt\uninstall.lnk
-  RmDir $SMPROGRAMS\mp3splt
+  Delete $SMPROGRAMS\mp3splt-gtk\mp3splt-gtk_doc.lnk
+  Delete $SMPROGRAMS\mp3splt-gtk\libmp3splt_doc.lnk
+  Delete $SMPROGRAMS\mp3splt-gtk\Mp3splt-gtk.lnk
+  Delete $SMPROGRAMS\mp3splt-gtk\uninstall.lnk
+  RmDir $SMPROGRAMS\mp3splt-gtk
 
   ;desktop shortcut section
-  Delete $DESKTOP\mp3splt.lnk
+  Delete $DESKTOP\mp3splt-gtk.lnk
 
   ;delete remaining ashes if possible
   Delete $INSTDIR\${PROGRAM_NAME}_uninst.exe
@@ -345,7 +406,6 @@ Function .onInit
 FunctionEnd
 ' >> $WIN_INSTALLER_FILE
 
-
 rm -f $TMP_GENERATED_FILES_FILE
 rm -f $TMP_CREATED_DIRECTORIES_FILE
 
@@ -355,6 +415,9 @@ else
   makensis win32_installer.nsi || exit 1
 fi
 
-#remove '.nsi' script
-rm -f $WIN_INSTALLER_FILE
+#remove .nsi script
+#rm -f $WIN_INSTALLER_FILE
+
+#remove used dirs
+rm -rf fr_locales && cd ../.. && rm -rf mp3splt-gtk_runtime
 
