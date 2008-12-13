0b29eba6969bf6c9df3395e342bde6dc3a0b8891
Alexandru Munteanu <m@ioalex.net>
2008-12-13 23:01:01 +0000

    fixed issues when creating the windows installer from mingw using the current documentation instructions from the website

--- compile_win32.sh
+++ compile_win32.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-#we move in the current script directory
-#!readlink not in mingw
-#script_dir=$(readlink -f $0)
-#script_dir=${script_dir%\/*.sh}
-#cd $script_dir/
-
 echo "unarchiving libmp3splt required libs ..."
 tar jxf ../libmp3splt_mingw_required_libs.tar.bz2 -C /
 cp /usr/bin/mingw32-make.exe /usr/bin/make.exe
