ac9041050e1f78ad1570742dd203ef17f7afc825
Alexandru Munteanu <m@ioalex.net>
2009-05-08 16:40:10 +0000

    optimized bash test framework

--- tests/constants_variables.sh
+++ tests/constants_variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DIFF_CMD="acoc diff"
+DIFF_CMD="diff"
+VIEW_DIFF_CMD="acoc diff"
 USE_GRAPHIC_DIFF=0
 GRAPHIC_DIFF_CMD="meld"
 
@@ -10,11 +11,12 @@ MP3_FILE="songs/La_Verue__Today.mp3"
 OUTPUT_DIR="output"
 ACTUAL_FILE="/tmp/mp3splt_actual.txt"
 EXPECTED_FILE="/tmp/mp3splt_expected.txt"
+COM_ACTUAL_FILE="/tmp/mp3splt_com_actual.txt"
+COM_EXPECTED_FILE="/tmp/mp3splt_com_expected.txt"
 TEMP_FILE="/tmp/mp3splt_temp.txt"
-NULL_FILE="/dev/null"
 
-expected=""
 test_name=""
 current_file=""
 current_tags_version=0
+command_output=""
 
--- tests/tests.sh
+++ tests/tests.sh
@@ -13,11 +13,20 @@ function test_normal
   M_FILE="La_Verue__Today"
 
   tags_option="-$current_tags_version"
-  if [[ $current_tags_version -eq -1 ]];then
+  if [[ $current_tags_version -eq 0 ]];then
     tags_option=""
     current_tags_version=2
+  elif [[ $current_tags_version -eq -1 ]];then
+    tags_option="-n"
+    test_name="$test_name & no tags"
   else
     test_name="$test_name & id3v$current_tags_version"
+  fi
+
+  #eyeD3 prints Rock (id 17) and not Rock (17)
+  id_str=""
+  if [[ $current_tags_version -eq 2 ]];then
+    id_str="id "
   fi
 
   expected=" Processing file 'songs/${M_FILE}.mp3' ...\n\
@@ -26,33 +35,44 @@ function test_normal
  info: starting normal split\n\
    File \"output/${M_FILE}_01m_00s__02m_00s.mp3\" created\n\
    File \"output/${M_FILE}_02m_00s__03m_05s.mp3\" created\n\
- Processed 7083 frames - Sync errors: 0\n\
- file split"
-  run_check_output "$tags_option -d $OUTPUT_DIR $MP3_FILE 1.0 2.0 3.5"
+   File \"output/${M_FILE}_03m_05s__04m_05s_58h.mp3\" created\n\
+ Processed 9402 frames - Sync errors: 0\n\
+ file split (EOF)"
+  run_check_output "$tags_option -d $OUTPUT_DIR $MP3_FILE 1.0 2.0 3.5 EOF" "$expected"
 
   current_file="$OUTPUT_DIR/${M_FILE}_01m_00s__02m_00s.mp3" 
-
   check_current_mp3_length "1.00"
-
-  #eyeD3 prints Rock (id 17) and not Rock (17)
-  id_str=""
-  if [[ $current_tags_version -eq 2 ]];then
-    id_str="id "
+  if [[ $current_tags_version -eq -1 ]];then
+    check_current_mp3_no_tags
+  else
+    check_all_current_mp3_tags "La Verue" "Riez Noir" "Today"\
+      "2007" "Rock (${id_str}17)" "1" "http://www.jamendo.com/"
   fi
-
-  check_all_current_mp3_tags "La Verue" "Riez Noir" "Today"\
-  "2007" "Rock (${id_str}17)" "1" "http://www.jamendo.com/"
 
   current_file="$OUTPUT_DIR/${M_FILE}_02m_00s__03m_05s.mp3" 
   check_current_mp3_length "1.05"
+  if [[ $current_tags_version -eq -1 ]];then
+    check_current_mp3_no_tags
+  else
+    check_all_current_mp3_tags "La Verue" "Riez Noir" "Today"\
+      "2007" "Rock (${id_str}17)" "2" "http://www.jamendo.com/"
+  fi
 
-  check_all_current_mp3_tags "La Verue" "Riez Noir" "Today"\
-  "2007" "Rock (${id_str}17)" "2" "http://www.jamendo.com/"
+  current_file="$OUTPUT_DIR/${M_FILE}_03m_05s__04m_05s_58h.mp3" 
+  check_current_mp3_length "1.00"
+  if [[ $current_tags_version -eq -1 ]];then
+    check_current_mp3_no_tags
+  else
+    check_all_current_mp3_tags "La Verue" "Riez Noir" "Today"\
+      "2007" "Rock (${id_str}17)" "3" "http://www.jamendo.com/"
+  fi
 
   p_green "PASSED"
   echo
 }
 
+function test_normal_original_tags { test_normal 0; }
+function test_normal_no_tags { test_normal -1; }
 function test_normal_id3v1 { test_normal 1; }
 function test_normal_id3v2 { test_normal 2; }
 
@@ -62,9 +82,12 @@ function run_normal_mode_tests
   echo
   p_blue " NORMAL tests ..."
   echo
-  test_normal -1
+
+  test_normal_original_tags
   test_normal_id3v1
   test_normal_id3v2
+  test_normal_no_tags
+
   p_blue " NORMAL tests DONE."
   echo
 }
@@ -82,4 +105,6 @@ p_cyan "\tStart date : $start_date"
 echo
 p_cyan "\tEnd date : $end_date"
 echo -e '\n'
+
+exit 0
 
