09db1994f0d4dfe2b426d7b758ce084b5e5d920f
io_alex_2004 <io_alex_2004@15f1896c-5e77-45dc-b76d-d072f9a9b3a2>
2009-05-30 16:48:59 +0000

    updated scripts for windows cross compilation

--- mp3splt-project/trunk/scripts/compile.sh
+++ mp3splt-project/trunk/scripts/compile.sh
@@ -394,9 +394,9 @@ update_versions
 ###################################
 #build packages
 
-source_packages
-ubuntu_packages
-debian_packages
+#source_packages
+#ubuntu_packages
+#debian_packages
 windows_cross_installers
 
 #gnu_linux_static_packages
