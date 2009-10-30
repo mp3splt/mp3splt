3c874befe7cb1fe7f5efc05fe7d5ee197763a2c0
io_alex_2004 <io_alex_2004@15f1896c-5e77-45dc-b76d-d072f9a9b3a2>
2009-10-30 23:53:24 +0000

    0.5.8 & 2.2.7 released - tests commit

--- mp3splt-project/tests/cue_mode_tests.sh
+++ mp3splt-project/tests/cue_mode_tests.sh
@@ -4,7 +4,7 @@
 
 function _create_cue_file
 {
-  Q=$1
+  Q='"'
   echo "PERFORMER ${Q}GNU_Linux${Q}
 TITLE ${Q}Gentoo${Q}
 FILE ${Q}$MP3_FILE${Q} MP3
@@ -14,6 +14,25 @@ FILE ${Q}$MP3_FILE${Q} MP3
     INDEX 01 00:00:00
   TRACK 02 AUDIO
     TITLE ${Q}Our guitar${Q}
+    INDEX 01 01:43:00
+  TRACK 03 AUDIO
+    TITLE ${Q}Our laptop${Q}
+    PERFORMER ${Q}Third performer${Q}
+    INDEX 01 03:20:00
+" > $CUE_FILE
+}
+
+function _create_incomplete_cue_file
+{
+  Q=$1
+  echo "PERFORMER ${Q}GNU_Linux${Q}
+TITLE ${Q}Gentoo${Q}
+FILE ${Q}$MP3_FILE${Q} MP3
+  TRACK 01 AUDIO
+    TITLE ${Q}Our piano${Q}
+    PERFORMER ${Q}First performer${Q}
+    INDEX 01 00:00:00
+  TRACK 02 AUDIO
     INDEX 01 01:43:00
   TRACK 03 AUDIO
     TITLE ${Q}Our laptop${Q}
@@ -86,6 +105,59 @@ $auto_adjust_warning"
 
 function test_cue_mode { _test_cue_mode; }
 function test_cue_mode_with_quotes { _test_cue_mode \"; }
+
+function test_cue_mode_incomplete
+{
+  _create_incomplete_cue_file
+
+  remove_output_dir
+
+  M_FILE="La_Verue__Today"
+
+  test_name="cue mode incomplete"
+
+  expected=" Processing file 'songs/${M_FILE}.mp3' ...
+ reading informations from CUE file songs/test.cue ...
+
+  Artist: GNU_Linux
+  Album: Gentoo
+  Tracks: 3
+
+ cue file processed
+ info: file matches the plugin 'mp3 (libmad)'
+ info: found Xing or Info header. Switching to frame mode... 
+ info: MPEG 1 Layer 3 - 44100 Hz - Joint Stereo - FRAME MODE - Total time: 4m.05s
+ info: starting normal split
+   File \"$OUTPUT_DIR/First performer - 1 - Our piano.mp3\" created
+   File \"$OUTPUT_DIR/GNU_Linux - 2 - .mp3\" created
+   File \"$OUTPUT_DIR/Third performer - 3 - Our laptop.mp3\" created
+ Processed 9402 frames - Sync errors: 0
+ file split (EOF)
+$auto_adjust_warning"
+  mp3splt_args="-d $OUTPUT_DIR -c $CUE_FILE $MP3_FILE" 
+  run_check_output "$mp3splt_args" "$expected"
+
+  current_file="$OUTPUT_DIR/First performer - 1 - Our piano.mp3"
+  check_current_mp3_length "01.43"
+  check_current_file_has_xing
+  check_all_mp3_tags_with_version "2" "First performer" "Gentoo" "Our piano"\
+  "None" "Other" "12" "1" ""
+
+  current_file="$OUTPUT_DIR/GNU_Linux - 2 - .mp3"
+  check_current_mp3_length "01.37"
+  check_current_file_has_xing
+  check_all_mp3_tags_with_version "2" "GNU_Linux" "Gentoo" ""\
+  "None" "Other" "12" "2" ""
+
+  current_file="$OUTPUT_DIR/Third performer - 3 - Our laptop.mp3"
+  check_current_mp3_length "00.45"
+  check_current_file_has_xing
+  check_all_mp3_tags_with_version "2" "Third performer" "Gentoo" "Our laptop"\
+  "None" "Other" "12" "3" ""
+
+  p_green "OK"
+  echo
+}
 
 function test_cue_mode_quiet
 {
