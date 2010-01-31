7058ad5c28382859dd1261594bce9e92aceb28ca
io_alex_2004 <io_alex_2004@15f1896c-5e77-45dc-b76d-d072f9a9b3a2>
2010-01-31 21:03:41 +0000

    fixed important problem with ogg comments not written in the output file

--- mp3splt-project/trunk/newmp3splt/src/mp3splt.c
+++ mp3splt-project/trunk/newmp3splt/src/mp3splt.c
@@ -46,7 +46,7 @@
 #define PACKAGE_NAME "mp3splt"
 #endif
 
-#define MP3SPLT_DATE "17/12/09"
+#define MP3SPLT_DATE "31/01/10"
 #define MP3SPLT_AUTHOR1 "Matteo Trotta"
 #define MP3SPLT_AUTHOR2 "Alexandru Munteanu"
 #define MP3SPLT_EMAIL1 "<mtrotta AT users.sourceforge.net>"
