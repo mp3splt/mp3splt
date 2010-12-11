321b7baa2c995bb4d8574cee957b31b3c7418160
Alexandru Munteanu <m@ioalex.net>
2010-12-11 22:09:36 +0000

    fixed 'make check' issue - don't run cutter tests if the cutter command is not found

--- libmp3splt/test/run-tests.sh
+++ libmp3splt/test/run-tests.sh
@@ -11,5 +11,15 @@ if test -z "$CUTTER"; then
     CUTTER="`make -s -C $BASE_DIR echo-cutter`"
 fi
 
+($CUTTER --version) > /dev/null 2>&1 ||
+{
+  echo 1>&2
+  echo "Error: '$CUTTER' not found." 1>&2
+  echo "       Please install the cutter testing framework or configure with '--disable-cutter'" 1>&2
+  echo 1>&2
+
+  exit 1
+}
+
 $CUTTER -s $BASE_DIR "$@" $BASE_DIR
 
