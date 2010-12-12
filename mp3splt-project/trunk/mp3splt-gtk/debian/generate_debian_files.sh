2e6c79a0969a30c011128f6c78acb7d06262468e
io_alex_2004 <io_alex_2004@15f1896c-5e77-45dc-b76d-d072f9a9b3a2>
2010-12-12 00:10:35 +0000

    put back make_debian_package.sh & generate_debian_package.sh - removed outputdirectory_get() from output_format (this is not the output directory) - removed '-dev' dependencies for libmp3splt debian package - fixed libmp3splt 'test' dir 'make dist'

---
+++ mp3splt-project/trunk/mp3splt-gtk/debian/generate_debian_files.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#this scripts generates the "control" and "docs" files
+
+#we move in the current script directory
+script_dir=$(readlink -f $0) || exit 1
+script_dir=${script_dir%\/*.sh}
+cd $script_dir
+
+. ../include_variables.sh "quiet_noflags" || exit 1
+
+#generate the control file
+echo "Source: mp3splt-gtk
+Section: sound
+Priority: optional
+Maintainer: Ryan Niebur <ryanryan52@gmail.com>
+DM-Upload-Allowed: yes
+Build-Depends: debhelper (>= 7.2), libmp3splt-dev (>= ${LIBMP3SPLT_VERSION}), libgtk2.0-dev, libgstreamer0.10-dev, libgstreamer-plugins-base0.10-dev, audacious-dev
+Standards-Version: 3.9.1
+Homepage: http://mp3splt.sourceforge.net/
+Vcs-Git: git://git.ryan52.info/git/mp3splt-gtk
+Vcs-Browser: http://git.ryan52.info/?p=mp3splt-gtk;a=summary
+
+Package: mp3splt-gtk
+Architecture: any
+Depends: \${shlibs:Depends}, \${misc:Depends}, libmp3splt-mp3 | libmp3splt-plugin
+Recommends: gstreamer0.10-plugins-good
+Suggests: audacious
+Description: GTK2 gui that uses libmp3splt for lossless linear editing
+ of audio files (cut an audio file into several tracks.)
+ Since there is no decoding step involved this means that MP3 (including
+ VBR) and Ogg Vorbis can be split into smaller files without
+ loosing any quality. Useful for splitting albums into tracks which
+ can be done either manually, using freedb.org data from the net
+ or using .cue files ..." > control
+
+#generate the docs file
+counter=1;
+for doc in "${MP3SPLT_GTK_DOC_FILES[@]}";do
+    if [[ $doc != "COPYING" ]];then
+        if [[ $counter == 1 ]];then
+            echo $doc > ./docs
+            counter=2
+        else
+            echo $doc >> ./docs
+        fi
+    fi
+done
