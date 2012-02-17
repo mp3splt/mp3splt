b65c63310dac85f292bee9cf66dd580b3eafcc1d
io_alex_2004 <io_alex_2004@15f1896c-5e77-45dc-b76d-d072f9a9b3a2>
2012-02-17 21:23:27 +0000

    build snapshot 970

--- mp3splt-project/trunk/newmp3splt/src/common.h
+++ mp3splt-project/trunk/newmp3splt/src/common.h
@@ -43,7 +43,7 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #else
-#define VERSION "2.4.1.966"
+#define VERSION "2.4.1.970"
 #define PACKAGE_NAME "mp3splt"
 #endif
 
@@ -58,7 +58,7 @@
 #  define _(STR) ((const char *)STR)
 #endif
 
-#define MP3SPLT_DATE "10/01/12"
+#define MP3SPLT_DATE "14/02/12"
 #define MP3SPLT_AUTHOR1 "Matteo Trotta"
 #define MP3SPLT_AUTHOR2 "Alexandru Munteanu"
 #define MP3SPLT_EMAIL1 "<mtrotta AT users.sourceforge.net>"
