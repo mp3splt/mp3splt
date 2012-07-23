7364f434452a1dbfbbaeb535ff296b55b6eb45d2
Alexandru Munteanu <m@ioalex.net>
2012-07-23 19:53:52 +0000

    small fix when using gtk+2

--- mp3splt-gtk/src/split_mode_window.c
+++ mp3splt-gtk/src/split_mode_window.c
@@ -437,7 +437,7 @@ GtkWidget *create_special_split_page(ui_state *ui)
   GtkWidget *vbox = wh_vbox_new();;
 
 #if GTK_MAJOR_VERSION <= 2
-  GtkWidget *paned_widget = gtk_hpaned_new();
+  GtkWidget *paned_widget = gtk_vpaned_new();
 #else
   GtkWidget *paned_widget = gtk_paned_new(GTK_ORIENTATION_VERTICAL);
 #endif
