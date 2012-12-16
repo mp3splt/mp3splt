a813e9e2dd5300e14f5a8028053cf12caf85ed61
io_alex_2004 <io_alex_2004@15f1896c-5e77-45dc-b76d-d072f9a9b3a2>
2012-12-16 15:46:47 +0000

    build 1160

--- mp3splt-project/trunk/newmp3splt/include_variables.sh
+++ mp3splt-project/trunk/newmp3splt/include_variables.sh
@@ -8,10 +8,10 @@
 #build architecture
 ARCH=${ARCH:=`uname -m`}
 #mp3splt version
-MP3SPLT_VERSION=2.4.4.1158
+MP3SPLT_VERSION=2.4.4.1160
 #the libmp3splt version (if mp3splt is only compatible with this
 #libmp3splt version)
-LIBMP3SPLT_VERSION=1.0.1158
+LIBMP3SPLT_VERSION=1.0.1160
 #the mp3splt documentation files
 MP3SPLT_DOC_FILES=(AUTHORS ChangeLog COPYING INSTALL NEWS README TODO)
 #the mp3splt installed files (needed for openbsd, freebsd, ...)
