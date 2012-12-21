378a8fb8e9e6a70ec99877c10fcf6b156bcba5ae
io_alex_2004 <io_alex_2004@15f1896c-5e77-45dc-b76d-d072f9a9b3a2>
2012-12-21 22:37:53 +0000

    build 1166

--- mp3splt-project/trunk/newmp3splt/src/common.h
+++ mp3splt-project/trunk/newmp3splt/src/common.h
@@ -43,7 +43,7 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #else
-#define VERSION "2.4.4.1160"
+#define VERSION "2.4.4.1166"
 #define PACKAGE_NAME "mp3splt"
 #endif
 
@@ -58,7 +58,7 @@
 #  define _(STR) ((const char *)STR)
 #endif
 
-#define MP3SPLT_DATE "16/12/12"
+#define MP3SPLT_DATE "21/12/12"
 #define MP3SPLT_AUTHOR1 "Matteo Trotta"
 #define MP3SPLT_AUTHOR2 "Alexandru Munteanu"
 #define MP3SPLT_EMAIL1 "<mtrotta AT users.sourceforge.net>"
