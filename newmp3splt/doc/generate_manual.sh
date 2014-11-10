b1567c2ea4ef4a45e934c70bf1d45e52d54df0b3
Alexandru Munteanu <m@ioalex.net>
2014-11-10 22:09:20 +0000

    versions 0.9.2 & 2.6.2 released

--- newmp3splt/doc/generate_manual.sh
+++ newmp3splt/doc/generate_manual.sh
@@ -9,7 +9,7 @@ MANUAL_FILE=manual.html
 
 man2html ./mp3splt.1 > $MANUAL_FILE
 
-sed -i 's/^Content-type: text\/html$//' $MANUAL_FILE
+sed -i 's/^Content-type: text\/html.*$//' $MANUAL_FILE
 sed -i 's/^Section: User Commands  (1)<BR><A HREF="#index">Index<\/A>//' $MANUAL_FILE
 sed -i 's/^<A HREF="\/cgi-bin\/man\/man2html">Return to Main Contents<\/A>//' $MANUAL_FILE
 
